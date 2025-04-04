58a1a970dc607ecb9dc0003bdaec9344319d7087
edison <edisoncaratar222@gmail.com>
2025-04-03 20:23:12 -0500

    realizamos el ingreso de datos y miaramos si los datos ya estan registrados

--- src/signup.php
+++ src/signup.php
@@ -1,22 +1,48 @@
 <?php
 
-    include('config/database.php');
+include('config/database.php');
 
-    $fname =    $_POST['f_name']; 
-    $lname =    $_POST['l_name']; 
-    $email =    $_POST['e_mail']; 
-    $passw =    $_POST['p_assw']; 
+    $fname = $_POST['f_name'];
+    $lname = $_POST['l_name'];
+    $email = $_POST['e_mail'];
+    $passw = $_POST['p_assw'];
 
-    $sql = "INSERT INTO users
-            (firstname, lastnmae, email, password)
-            VALUES('$fname','$lnmae','$email','$passw')
 
+
+    $sql_validate_email = "
+            select 
+            count(id) as total
+        from
+            users
+        where 
+            email= '$email' and
+            status = true
+    " ;
+
+    $ans =pg_query($conn, $sql_validate_email);
+
+    if ($ans){//$ans == true
+        $row =pg_fetch_assoc($ans);
+        if ($row['total'] > 0){
+            echo "el usuario ya existe";
+        }
+        else{
+            $sql = "INSERT INTO users
+                (firstname, lastname, email, password)
+            values ('$fname','$lname','$email','$passsw')
             ";
-            $ans = pg_query($conn, $sql);
-            if($znx){
-                echo 'users has been created successfully';
+
+            $ans = pg_query($conn,$sql);
+            if ($ans){
+                echo'ya fue ingresado';
             }else{
-                echo 'error' ;
+                echo'error';
             }
+        }
+    }else{
+        echo 'no existe';
+    }
+
+
 
 ?>
